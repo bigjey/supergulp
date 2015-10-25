9768493f859d95373bb93930cd347671ba7c6ccd
Ден bigjey Ижецкий <mail.bigjey@gmail.com>
2015-10-26 01:40:58 +0300

    jsons

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var wiredep = require('wiredep').stream;
+var gulp = require('gulp'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rimraf = require('gulp-rimraf'),
+    gulpFilter = require('gulp-filter'),
+    wiredep = require('wiredep').stream,
 
-var path {
+    mainBowerFiles = require('gulp-main-bower-files');;
+
+var path = {
   dev: {
     html: './dev/html/*.html',
     scss: './dev/static/scss/**/*.scss',
@@ -19,4 +27,33 @@ gulp.task('bower', function () {
     .pipe(wiredep({
     }))
     .pipe(gulp.dest( path.dev.wiredepFolder ));
+});
+
+gulp.task('clean', function(){
+
+  gulp.src( './prod/**/*' )
+    .pipe( rimraf() );
+
+})
+
+gulp.task('bower_build', function(){
+  var jsOnly = gulpFilter(['**/*.js'], { restore: true } ),
+      cssOnly = gulpFilter(['**/*.css'], { restore: true } );
+
+  return gulp.src('./bower.json')
+    .pipe( mainBowerFiles( ) )
+
+    .pipe( jsOnly )
+    .pipe( uglify() )
+    .pipe( concat( 'plugins.js' ) )
+    .pipe( gulp.dest('./prod/') )
+    .pipe( jsOnly.restore )
+
+    .pipe( cssOnly )
+    .pipe( concat( 'plugins.css' ) )
+    .pipe( minifyCss( {
+      keepSpecialComments: 0
+    }) )
+    .pipe( gulp.dest( './prod/' ) );
+
 });
